dc2f417244d470560f2af346019b209d741df1e6
cs25m205-coder <cs25m205@iittp.ac.in>
2026-03-18 19:47:03 +0530

    added epsilon-greedy

--- ba_hw2.py
+++ ba_hw2.py
@@ -86,13 +86,32 @@ def epsilon_greedy(bandit, T, epsilon=0.1):
     exploit the current best arm otherwise."""
     K = bandit.K 
     #Initialize algorithm dependent variable here
+    no_of_pulls = np.zeros(K)
+    emp_mean = np.zeros(K)
     rewards, regrets = [], []
     cumulative_regret = 0
 
     # Initialise: pull each arm once
     for arm in range(K):
-
+        reward = bandit.pull(arm)
+        no_of_pulls[arm] += 1
+        emp_mean[arm] = reward
+        rewards.append(reward)
+        regret = bandit.best_mean - bandit.means[arm]
+        cumulative_regret += regret
+        regrets.append(cumulative_regret)
     for t in range(K, T):
+        if np.random.rand() < epsilon:
+            arm = np.random.randint(K)
+        else:
+           arm = np.argmax(emp_mean)
+        reward = bandit.pull(arm)
+        no_of_pulls[arm] += 1
+        emp_mean[arm] += (reward - emp_mean[arm]) / no_of_pulls[arm]
+        rewards.append(reward)
+        regret = bandit.best_mean - bandit.means[arm]
+        cumulative_regret += regret
+        regrets.append(cumulative_regret)
 
     return np.array(rewards), np.array(regrets)
 
